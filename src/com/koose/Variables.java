aab9aaa7d32f33016b7f3c1e186b655af0de6bcd
Koose <KooseTheBlogger@gmail.com>
2022-07-21 04:12:20 +0000

    Intergers Variables

---
+++ src/com/koose/Variables.java
@@ -0,0 +1,27 @@
+package com.koose;
+
+public class Variables {
+
+    public static void main(String[] args){
+        int P;
+        int K;
+        int O;
+        int X;
+        int Y;
+
+        P = 89;
+        K = 3;
+        O = 8;
+        X = 4;
+        Y = 3;
+
+
+        System.out.println(P + O);
+        System.out.println(K - O);
+        System.out.println(P * K);
+        System.out.println(X/Y);
+
+
+
+    }
+}
