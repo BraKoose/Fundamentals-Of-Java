fb6d0ab264c84bb7c6b9746a80d98bb59119f3c1
Koose <KooseTheBlogger@gmail.com>
2022-07-23 07:28:26 +0000

    characters

---
+++ src/com/koose/Day2/Char.java
@@ -0,0 +1,25 @@
+package com.koose.Day2;
+
+public class Char {
+
+    public static void main(String[] agrs){
+        char character1 = 72;
+        char character2 = 'e';
+        char character3 = 'l';
+        char character4 = 'l';
+        char character5 = 81;
+
+
+        System.out.print(character1);
+        System.out.print(character2);
+        System.out.print(character3);
+        System.out.print(character4);
+        System.out.println(character5);
+
+
+
+
+
+
+    }
+}
