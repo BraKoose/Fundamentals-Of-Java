4b2de64866ee098f2a9482a356fa018d60af14af
Koose <KooseTheBlogger@gmail.com>
2022-07-23 08:04:51 +0000

    String easily concatenated

---
+++ src/com/koose/Day2/NonString.java
@@ -0,0 +1,14 @@
+package com.koose.Day2;
+
+public class NonString {
+
+    //String is not a primitive type but a class Type
+    public static void main(String [] args){
+        char s = 'k';
+        String o = "Bra";
+        String p = "Koose";
+        String s3 = o + " "+ p;
+
+        System.out.println(s3);
+    }
+}
